7cbcdbd370444bce2aa2dc96ccc5da4c0e8d330c
am <am>
2025-07-22 07:36:26 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'b.owtech.islamic.apps.quranmadinah',
+  appName: 'Quran Madinah',
+  webDir: 'dist'
+};
+
+export default config;
